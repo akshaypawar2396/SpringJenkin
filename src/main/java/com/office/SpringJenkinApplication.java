cf66a2a5b17de38ce6705465b46cf06f630070d5
Akshay Pawar <akshaypawar.5146@gmail.com>
2022-06-09 10:48:10 +0530

    first commit

---
+++ src/main/java/com/office/SpringJenkinApplication.java
@@ -0,0 +1,27 @@
+package com.office;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinApplication {
+
+
+public static Logger logger=LoggerFactory.getLogger(SpringJenkinApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application Started .....");
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinApplication.class, args);
+		logger.info("Application Executed...");
+	}
+
+}
